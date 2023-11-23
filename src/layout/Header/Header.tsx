2b4aee2766b2201847905d57d2d7519fc7edf5d2
koreaygj <ygj1828@gmail.com>
2023-11-24 02:08:40 +0900

    Fix: header title

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import AddNoteModal from "../../components/Modal/AddNoteModal/AddNoteModal";
 import { useAppDispatch, useAppSelector } from "../../hooks/redux";
 import { toggleAddNoteModal } from "../../store/modal/modalSlice";
@@ -6,13 +6,13 @@ import { toggleAddNoteModal } from "../../store/modal/modalSlice";
 function Header() {
   const { viewAddNoteModal } = useAppSelector((state) => state.modal);
   const dispatch = useAppDispatch();
-  const { pathname } = useLocation();
-  if (pathname === "/404") {
+  const { name } = useParams() as { name: string };
+  if (name === "/404") {
     return null;
   }
   return (
     <div>
-      <h1>{pathname.slice(1) === "" ? "Home" : pathname.slice(1)}</h1>
+      <h1>{name === undefined ? "Home" : name}</h1>
       <button onClick={() => dispatch(toggleAddNoteModal(true))}>
         새로운 노트
       </button>
