1448256500499371063feadb6f0af63e1b4c0cf5
koreaygj <ygj1828@gmail.com>
2023-11-24 16:47:51 +0900

    feat: add main note list in note modal

--- src/components/Modal/AddNoteModal/AddNoteModal.tsx
+++ src/components/Modal/AddNoteModal/AddNoteModal.tsx
@@ -7,6 +7,9 @@ import {
 import { useAppDispatch, useAppSelector } from "../../../hooks/redux";
 import TagModal from "../TagModal/TagModal";
 import TextEditor from "../../TextEditor/TextEditor";
+import { Note } from "../../../types/notes";
+import { toast } from "react-toastify";
+import { setMainNotes } from "../../../store/noteList/noteListSlice";
 
 function AddNoteModal() {
   const { viewAddTagsModal } = useAppSelector((state) => state.modal);
@@ -20,10 +23,6 @@ function AddNoteModal() {
 
   const dispatch = useAppDispatch();
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-  };
-
   const handleUpdateTag = (tag: string, type: string) => {
     const newTag = tag;
     if (type === "add") {
@@ -31,6 +30,42 @@ function AddNoteModal() {
     } else {
       setUpdatedTags(updatedTags.filter(({ name }) => name !== newTag));
     }
+  };
+
+  const date = Date.now().toString();
+
+  const createNote = () => {
+    if (!title) {
+      toast.error("타이틀을 적어주세요.");
+      return;
+    } else if (content === "<p><br></p>") {
+      toast.error("글을 작성해주세요.");
+      return;
+    }
+    let note: Partial<Note> = {
+      title: title,
+      content: content,
+      tags: updatedTags,
+      color: backgroundColor,
+      priority,
+      editedTime: new Date().getTime(),
+    };
+
+    if (editNote) {
+      note = { ...editNote, ...note };
+    } else {
+      note = {
+        ...note,
+        date,
+        createdTime: new Date().getTime(),
+        editedTime: null,
+        isPinned: false,
+        isRead: false,
+        id: Date.now(),
+      };
+    }
+    dispatch(setMainNotes(note));
+    dispatch(toggleAddNoteModal(false));
   };
 
   return (
@@ -43,7 +78,7 @@ function AddNoteModal() {
         />
       )}
       <div className={styles.modal}>
-        <form onSubmit={handleSubmit}>
+        <div>
           <div className={styles.modalHeader}>
             <h3>노트 생성하기</h3>
             <button onClick={() => dispatch(toggleAddNoteModal(false))}>
@@ -102,9 +137,11 @@ function AddNoteModal() {
             </div>
           </div>
           <div className={styles.modalFooter}>
-            <button type="submit">생성하기</button>
+            <button type="submit" onClick={createNote}>
+              생성하기
+            </button>
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
