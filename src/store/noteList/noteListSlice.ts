08f82e0e9e3c65fb93c1116afea97fd41b4558ef
koreaygj <ygj1828@gmail.com>
2023-11-27 16:59:07 +0900

    feat: setEdit & moveToTrash & removeNotes actions

--- src/store/noteList/noteListSlice.ts
+++ src/store/noteList/noteListSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Note } from "../../types/notes";
+import { toast } from "react-toastify";
 
 interface NoteState {
   mainNotes: Note[];
@@ -31,9 +32,57 @@ const noteListSlice = createSlice({
             note.id === payload.id ? payload : note
           )
         : state.mainNotes.push(payload);
+      toast.info("노트가 저장되었습니다");
+    },
+
+    setPinnedNotes: (state, { payload }) => {
+      state.mainNotes = state.mainNotes.map((note) =>
+        note.id === payload ? { ...note, isPinned: !note.isPinned } : note
+      );
+    },
+
+    setEditNote: (state, { payload }) => {
+      state.editNote = payload;
+    },
+
+    moveNoteToArchive: (state, { payload }) => {
+      if (!state.archiveNotes.find((note) => payload.id === note.id)) {
+        state.archiveNotes.push(payload);
+        state.mainNotes = state.mainNotes.filter(
+          (note) => note.id !== payload.id
+        );
+      }
+    },
+
+    moveNoteToTrash: (state, { payload }) => {
+      const { type, curNote } = payload;
+      if (type === noteType.archiveNotes) {
+        state.trashNotes.push(curNote);
+        state.archiveNotes = state.archiveNotes.filter(
+          (note) => note.id !== curNote.id
+        );
+      } else {
+        state.trashNotes.push(curNote);
+        state.mainNotes = state.mainNotes.filter(
+          (note) => note.id !== curNote.id
+        );
+      }
+    },
+
+    removeNotes: (state, { payload }) => {
+      state.trashNotes = state.trashNotes.filter(
+        (note) => note.id !== payload.curNote.id
+      );
     },
   },
 });
 
-export const {} = noteListSlice.actions;
+export const {
+  setMainNotes,
+  setPinnedNotes,
+  setEditNote,
+  moveNoteToArchive,
+  moveNoteToTrash,
+  removeNotes,
+} = noteListSlice.actions;
 export default noteListSlice.reducer;
