2d61f69d9bdbbe4493e41557ecfd722f2fe2d796
koreaygj <ygj1828@gmail.com>
2023-11-27 16:58:32 +0900

    fix: archive notes pages styles

--- src/routes/ArchiveNotes/ArchiveNotes.tsx
+++ src/routes/ArchiveNotes/ArchiveNotes.tsx
@@ -1,22 +1,9 @@
-import React, { useState } from "react";
 import MainWrapper from "../../components/MainWrapper/MainWrapper";
 import { useAppSelector } from "../../hooks/redux";
-import NewNoteModal from "../../components/Modal/AddNoteModal/AddNoteModal";
-import { toggleAddNoteModal } from "../../store/modal/modalSlice";
-import Header from "../../layout/Header/Header";
 
 function ArchiveNotes() {
   const { archiveNotes } = useAppSelector((state) => state.notesList);
-  return (
-    <div>
-      <Header />
-      {archiveNotes.length === 0 ? (
-        <div>노트가 없습니다.</div>
-      ) : (
-        <MainWrapper notes={archiveNotes} type="archive" />
-      )}
-    </div>
-  );
+  return <MainWrapper notes={archiveNotes} type="archiveNotes" />;
 }
 
 export default ArchiveNotes;
